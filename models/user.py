8912c8786356e5d8f42cdbd6d4e88a62dfb77f02
Prarup Gurung <grgprarup@gmail.com>
2024-02-10 14:19:04 +0545

    Updated user model: added fullname, confirmpassword

--- models/user.py
+++ models/user.py
@@ -4,5 +4,7 @@ from db import db
 class UserModel(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    confirm_password = db.Column(db.String(80), nullable=False)
