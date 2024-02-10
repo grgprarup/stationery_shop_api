2ea82ffa67f9cad9a7db6935aee87f7044defbe5
Prarup Gurung <grgprarup@gmail.com>
2024-02-10 14:18:43 +0545

    Updated user schema: added fullname, confirmpassword

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -3,10 +3,13 @@ from marshmallow import Schema, fields
 
 class UserSchema(Schema):
     id = fields.Str(dump_only=True)
+    full_name = fields.Str(required=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True, load_only=True)
+    confirm_password = fields.Str(required=True, load_only=True)
 
 
 class UserUpdateSchema(Schema):
+    full_name = fields.Str()
     username = fields.Str()
     password = fields.Str()
